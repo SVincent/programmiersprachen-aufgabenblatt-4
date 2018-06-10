0aae6a7964ff36db173fec347cbbf11e605515c5
Vincent Soellner <soellner.vincent@gmail.com>
2018-06-10 23:27:58 +0200

    initial commit

---
+++ build/source/CMakeFiles/aufgabe_14.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aufgabe_14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/source/CMakeFiles/aufgabe_2_und_3.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/aufgabe_2_und_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
