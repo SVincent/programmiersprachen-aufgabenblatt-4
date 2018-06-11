71020f76f4f6aa0f76c04774c1988cfbf718d70a
Vincent Soellner <soellner.vincent@gmail.com>
2018-06-11 09:38:17 +0200

    Task 4.3 added functions + tests

--- source/List.hpp
+++ source/List.hpp
@@ -1,6 +1,8 @@
 #ifndef BUW_LIST_HPP
 #define BUW_LIST_HPP
 #include <cstddef>
+
+using namespace std;
 // List.hpp
 
 //Why does template class List appear twice?
@@ -9,6 +11,18 @@ class List;
 
 template <typename T>
 struct ListNode {
+    ListNode():
+        value(),
+        prev(nullptr),
+        next(nullptr)
+        {}
+
+    ListNode(T const& v, ListNode* prev, ListNode* next):
+        value(v),
+        prev(prev),
+        next(next)
+        {}
+        
     T value = T();
     ListNode* prev = nullptr;
     ListNode* next = nullptr;
@@ -17,8 +31,7 @@ struct ListNode {
 template <typename T>
 class ListIterator {
 public:
-// not implemented yet
-// do not forget about the initialiser lists !
+
 private:
     ListNode <T >* node ;
 };
@@ -61,10 +74,86 @@ std::size_t size() const {
     return size_;
 }
 
+//Task 4.3
+
+void push_front(T const& value) {
+    ListNode<T>* node = new ListNode<T>{value, nullptr, nullptr};     
+    if (empty()) {
+        first_ = node;
+        last_ = node;
+    } 
+    else {
+        node->next = first_;
+        first_->prev = node;
+        first_ = node;
+    }
+    ++size_;
+}
+
+void push_back(T const& value) {
+    ListNode<T>* node = new ListNode<T>{value, nullptr, nullptr};
+    if (empty()) {
+        first_ = node;
+        last_ = node;
+    }
+    else {
+        node->prev = last_;
+        last_->next = node;
+        last_ = node;
+    }
+    ++size_;
+}
+
+void pop_front() {
+    if (empty()) {
+        //do nothing
+    }
+    else if (size() == 1) {
+        //turn into empty list
+        delete first_;
+        first_ = nullptr;
+        last_ = first_;
+        --size_;
+    }
+    else {
+        auto temp = first_->next;
+        delete first_;
+        first_ = temp;
+        first_->prev = nullptr;
+        --size_;
+    }
+}
 
 
-// not implemented yet
-// do not forget about the initialiser list !
+void pop_back() {
+    if (empty()) {
+        //do nothing
+    }
+    else if (size() == 1) {
+        //turn into empty list
+        delete first_;
+        first_ = nullptr;
+        last_ = first_;
+        --size_;
+    }
+    else {
+        auto temp = last_->prev;
+        delete last_;
+        temp->next = nullptr;
+        last_ = temp;
+        --size_; 
+    }
+}
+
+T front() const{
+    return first_->value;
+}
+
+T back() const{
+    return last_->value;
+}
+
+
 private:
     std::size_t size_;
     ListNode<T>* first_;
--- source/TestList.cpp
+++ source/TestList.cpp
@@ -5,13 +5,52 @@
 //Task 4.2
 
 TEST_CASE ("test for size of list", "Task 4.2") {
-    List<int> lista;
-    REQUIRE (lista.size()== 0);
+    List<int> list;
+    REQUIRE (list.size()== 0);
 }
 
 TEST_CASE ("test for empty list", "Task 4.2") {
-    List<int> listb;
-    REQUIRE (listb.empty()== true);
+    List<int> list;
+    REQUIRE (list.empty()== true);
+}
+
+//Task 4.3
+TEST_CASE ("test for push_front", "Task 4.3") {
+    List<int> list;
+    list.push_front(42);
+    REQUIRE (42 == list.front());
+    REQUIRE (list.size() == 1);
+}
+
+TEST_CASE ("test for push_back", "Task 4.3") {
+    List<int> list;
+    list.push_back(42);
+    REQUIRE (42 == list.back());
+    REQUIRE (list.size() == 1);
+}
+
+TEST_CASE ("test for pop_front", "Task 4.3") {
+    List<int> list;
+    list.push_front(42);
+    list.push_back(43);
+    REQUIRE (42 == list.front());
+    REQUIRE (list.size() == 2);
+    list.pop_front();
+    REQUIRE (43 == list.front());
+    list.pop_front();
+    REQUIRE (list.empty());
+}
+
+TEST_CASE ("test for pop_back", "Task 4.3") {
+    List<int> list;
+    list.push_front(42);
+    list.push_back(43);
+    REQUIRE (43 == list.back());
+    REQUIRE (list.size() == 2);
+    list.pop_back();
+    REQUIRE (42 == list.back());
+    list.pop_back();
+    REQUIRE (list.empty());
 }
 
 int main(int argc, char *argv[])
